8b332815767548b8a25ad44d07f55c6e759d31e5
TOM-BOSS-pinetour <b94889340@gmail.com>
2025-12-20 04:44:43 +0800

    updated chat propagation bug

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,12 +13,7 @@ export default function Home() {
 
   const toggleChat = () => setChatOpen(!chatOpen);
   return (
-    <div
-      onClickCapture={() => {
-        if (chatOpen) setChatOpen(false);
-      }}
-      className="relative"
-    >
+    <div className="relative">
       <div className="text-black font-sans text-[16px] font-bold py-4 px-12 flex justify-start flex-col border border-gray-500">
         AI tools
       </div>
@@ -56,12 +51,19 @@ export default function Home() {
         <MessageIcon />
       </div>
       {chatOpen && (
-        <div
-          className="fixed bottom-20 right-20 "
-          onClick={(e) => e.stopPropagation()}
-        >
-          <ChatBot onClose={() => setChatOpen(false)} />
-        </div>
+        <>
+          <div
+            className="fixed inset-0 z-40"
+            onClick={() => setChatOpen(false)}
+          />
+
+          <div
+            className="fixed bottom-20 right-20 z-50"
+            onClick={(e) => e.stopPropagation()}
+          >
+            <ChatBot onClose={() => setChatOpen(false)} />
+          </div>
+        </>
       )}
     </div>
   );
